3de0b6381963c8576b25c52f531d032a69329ba7
Инна <anni31717213@gmail.com>
2022-01-10 01:14:31 +0200

    дз 7 первая часть не закрывается модалка

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+let galleryEl = document.querySelector('.gallery');
+let getElements = document.querySelector('gallery__item');
+const bigPicture = galleryItems.map(
+    img => `<div class="gallery__item">
+    <a class="gallery__link" href=${img.original}>
+                    <img
+                    class="gallery__image"
+                    src=${img.preview}
+                    data-source=${img.original}
+                    alt=${img.description}
+                    />
+                </a>
+    </div>`,
+)
+    .join('');
+galleryEl.insertAdjacentHTML('beforeend', bigPicture);
 
-console.log(galleryItems);
+for (let index = 0; index < getElements.length; index++) {
+    getElements[i].addEventListener('click',
+        function (event) {
+            event.preventDefault();
+            return false;
+        });
+
+    const currentImg = getElements[i].dataset.sourse;
+    getElements[i].onclick = () => {
+        const libraryImg = basicLightbox.create(`<img width="1280" height="720" src=${currentImg}>`);
+        libraryImg.show();
+        document.addEventListener('keydown', closeModal);
+    
+        function closeModal(e) {
+            if (e.code === 'Escape') libraryImg.close();
+        };
+    };
+};
